e1ca50a326e04ba4bbb70040aadb613da0bb3a0e
Yohannes-ts <107062122+Yohannes-ts@users.noreply.github.com>
2022-07-13 14:02:39 -0500

    Update weekday.js

--- weekday.js
+++ weekday.js
@@ -1,21 +1,22 @@
 let today = "wednsday";
-if (today = monday){
+if (today === monday){
     console.log("today is monday")
 }
-    else if(today = tuesday){
+    else if(today=== tuesday){
         console.log("today is Tuesday")
     }
-    else if(today = wednsday){
+    else if(today === wednsday){
         console.log("today is wednsday")
     }
-    else if(today = thursday){
+    else if(today === thursday){
         console.log("today is thursday")
     }
-    else if(today = friday){
+    else if(today === friday){
         console.log("today is friday")
     }
-    else if(today = saturday){
+    else if(today === saturday){
         console.log("today is saturday")
     }
     else
     console.log("today is sunday")
+
