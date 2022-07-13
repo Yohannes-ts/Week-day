64d360b1a796aff42c033adc9b196f6b606e4548
Yohannes-ts <107062122+Yohannes-ts@users.noreply.github.com>
2022-07-13 09:33:30 -0500

    Create weekday.js

---
+++ weekday.js
@@ -0,0 +1,21 @@
+let today = "wednsday";
+if (today = monday){
+    console.log("today is monday")
+}
+    else if(today = tuesday){
+        console.log("today is Tuesday")
+    }
+    else if(today = wednsday){
+        console.log("today is wednsday")
+    }
+    else if(today = thursday){
+        console.log("today is thursday")
+    }
+    else if(today = friday){
+        console.log("today is friday")
+    }
+    else if(today = saturday){
+        console.log("today is saturday")
+    }
+    else
+    console.log("today is sunday")
